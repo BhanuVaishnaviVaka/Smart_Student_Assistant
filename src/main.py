065df4257cfa65ddc7de34d2a72bc5ca0a156a90
Bhanu Vaishnavi <bhanuvaka09@gmail.com>
2026-04-20 19:28:29 +0530

    Add Deadlines to the tasks

--- src/main.py
+++ src/main.py
@@ -1,29 +1,35 @@
-﻿def add_task(task):
-    with open("tasks.txt","a") as f:
-        f.write(task+ "\n")
-    print(f"Task added: {task}")
+﻿from datetime import datetime
+def add_task(task,deadline):
+    with open("tasks.txt","a",encoding="utf-8") as f:
+        f.write(f"{task}|{deadline}\n")
+    print(f"Task added: {task}(Deadline:{deadline})")
 def list_tasks():
     try:
-        with open("tasks.txt","r") as f:
+        with open("tasks.txt","r",encoding="utf-8") as f:
             tasks = f.readlines()
         if tasks:
-            print("Your Tasks: ")
+            print("\nYour Tasks: ")
             for i,task in enumerate(tasks, start=1):
                 print(f"{i}.{task.strip()}")
         else:
-            print("No Tasks Found.")
+            print("\nNo Tasks Found.")
     except FileNotFoundError:
         print("No Tasks File Found. Add a Task First!")
 def main():
     while True:
         print("\nSmart Student Assistant")
-        print("1.Add a Task")
+        print("1.Add a Task with deadline")
         print("2.List Tasks")
         print("3.Exit")
         choice=input("Choose an option: ")
         if choice=="1":
             task=input("Enter your Task: ")
-            add_task(task)
+            deadline=input("Enter deadline(YYYY-MM-DD.):")
+            try:
+                datetime.strptime(deadline, "%Y-%m-%d")
+                add_task(task,deadline)
+            except ValueError:
+                print("Invalid date format. Please use YYYY-MM-DD.")
         elif choice=="2":
             list_tasks()
         elif choice=="3":
